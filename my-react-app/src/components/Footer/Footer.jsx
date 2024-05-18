f835b2123a23fed0072bace42fa0f5b7142ed97c
Favio Sartori <faviop2000@gmail.com>
2024-05-18 14:24:57 -0500

    UnZip

---
+++ my-react-app/src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import './Footer.css';
+
+const Footer = () => { 
+    return (
+        <>
+            <nav>
+                <a href="#about">Sobre Nosotros</a>
+                <a href="#contact">Contacto</a>
+                <a href="#privacy">Política de Privacidad</a>
+            </nav>
+        </>
+    )
+}
+
+export default Footer;
