fe6b0aa8d793c0e2db4e35cc15924607a8ababf5
Carolina Ramirez De La Cuba <carolina.rdc@outlook.com>
2024-05-23 17:23:52 -0500

    fix: remove multiple router providers

--- src/main.jsx
+++ src/main.jsx
@@ -1,53 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import NotFound from './pages/NotFound.jsx'
-import ContactUs from './pages/ContactUs.jsx'
-import ListaSerie from './pages/ListaSerie.jsx'
-import AgregarSerie from './pages/AgregarSerie.jsx'
-import DatosRegistro from './pages/DatosRegistro.jsx'
-import ContraOlvi from './pages/ContraOlvi.jsx'
-import { CartPage } from './pages/CartPage'
-import {RouterProvider, createBrowserRouter } from 'react-router-dom'
+import App from "./App";
+import { CartProvider } from "./contexts/useCart";
 
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import './index.css'
+import "./index.css";
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <NotFound />
-  },
-  {
-    path:'/contactus',
-    element: <ContactUs />
-  },
-  {
-    path: '/listaserie',
-    element: <ListaSerie/>
-  },
-  {
-    path: '/cart',
-    element: <CartPage />
-  },
-  {
-    path: '/agregarserie',
-    element: <AgregarSerie/>
-  },
-  {
-    path:'/datosregistro',
-    element: <DatosRegistro/>
-  },
-  {
-    path:'/contraolvi',
-    element: <ContraOlvi/>
-  }
-])
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <CartProvider>
+      <App />
+    </CartProvider>
   </React.StrictMode>,
-)
-
+);
