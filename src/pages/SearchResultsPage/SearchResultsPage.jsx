6dbf586d3cbcc847bba6b8ad1422722174596382
Carolina Ramirez De La Cuba <carolina.rdc@outlook.com>
2024-05-23 22:13:20 -0500

    remove additional products constant
    incoming: products from admin

--- src/pages/SearchResultsPage/SearchResultsPage.jsx
+++ src/pages/SearchResultsPage/SearchResultsPage.jsx
@@ -1,35 +1,9 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
 
-import "./SearchResultsPage.css";
+import { PRODUCTS } from "../../constants/products";
 
-const productos = [
-  { id: 1, nombre: "Nike Air Max", marca: "Nike", precio: 120 },
-  { id: 2, nombre: "Adidas Ultraboost", marca: "Adidas", precio: 150 },
-  { id: 3, nombre: "Puma RS-X", marca: "Puma", precio: 100 },
-  { id: 4, nombre: "Reebok Classic Leather", marca: "Reebok", precio: 80 },
-  { id: 5, nombre: "New Balance 574", marca: "New Balance", precio: 90 },
-  { id: 6, nombre: "Vans Old Skool", marca: "Vans", precio: 70 },
-  {
-    id: 7,
-    nombre: "Converse Chuck Taylor All Star",
-    marca: "Converse",
-    precio: 60,
-  },
-  {
-    id: 8,
-    nombre: "Under Armour HOVR Phantom",
-    marca: "Under Armour",
-    precio: 130,
-  },
-  { id: 9, nombre: "Asics Gel-Kayano", marca: "Asics", precio: 160 },
-  { id: 10, nombre: "Skechers Go Walk", marca: "Skechers", precio: 75 },
-  { id: 11, nombre: "Fila Disruptor", marca: "Fila", precio: 110 },
-  { id: 12, nombre: "Salomon Speedcross", marca: "Salomon", precio: 140 },
-  { id: 13, nombre: "Brooks Ghost", marca: "Brooks", precio: 130 },
-  { id: 14, nombre: "Mizuno Wave Rider", marca: "Mizuno", precio: 140 },
-  { id: 15, nombre: "Saucony Jazz Original", marca: "Saucony", precio: 80 },
-];
+import "./SearchResultsPage.css";
 
 const useQuery = () => {
   return new URLSearchParams(useLocation().search);
@@ -53,13 +27,13 @@ const SearchResultsPage = () => {
     });
   };
 
-  const searchResults = productos
-    .filter((producto) => producto.marca.toLowerCase().includes(searchTerm))
-    .sort((a, b) =>
-      sortCriteria === "precio"
-        ? a.precio - b.precio
-        : a.nombre.localeCompare(b.nombre),
-    );
+  const searchResults = PRODUCTS.filter((producto) =>
+    producto.marca.toLowerCase().includes(searchTerm),
+  ).sort((a, b) =>
+    sortCriteria === "precio"
+      ? a.price - b.price
+      : a.nombre.localeCompare(b.nombre),
+  );
 
   return (
     <div className="search-results-container">
@@ -93,7 +67,7 @@ const SearchResultsPage = () => {
               <td>{result.id}</td>
               <td>{result.nombre}</td>
               <td>{result.marca}</td>
-              <td className="precio">${result.precio.toLocaleString()}</td>
+              <td className="precio">${result.price.toLocaleString()}</td>
             </tr>
           ))}
         </tbody>
